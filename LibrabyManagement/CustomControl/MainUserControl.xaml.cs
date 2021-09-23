bcb033e04d91b6496dec0495e12cfc2cfd805146
tdn352001 <tdn352001@gmail.com>
2021-09-23 11:14:40 +0700

    update UI Main

--- LibrabyManagement/CustomControl/MainUserControl.xaml.cs
+++ LibrabyManagement/CustomControl/MainUserControl.xaml.cs
@@ -14,7 +14,7 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace LibraryManagement.UserControls
+namespace LibraryManagement.CustomControl
 {
     /// <summary>
     /// Interaction logic for MainUserControl.xaml
