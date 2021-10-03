7d0709dfda0093f342868c7fce4a2b9dc44bdb63
tdn352001 <tdn352001@gmail.com>
2021-10-04 00:41:53 +0700

    update UI + connect to database

---
+++ LibraryManagement/ViewModel/StoreViewModel.cs
@@ -0,0 +1,27 @@
+﻿using LibraryManagement.Model;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace LibraryManagement.ViewModel
+{
+    public class StoreViewModel : BaseViewModel
+    {
+        
+        private ObservableCollection<BookStore> _StoreList { get; set; }
+        public ObservableCollection<BookStore> StoreList { get => _StoreList; set { _StoreList = value; OnPropertyChanged(); } }
+
+        public StoreViewModel()
+        {
+            
+        }
+        public StoreViewModel(LibraryManagementEntities db)
+        {
+
+        }
+    }
+}
