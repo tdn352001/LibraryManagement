d83bac675e1d825d0b5513f7a83150afbb9e933a
Phongvo123 <19522015@gm.uit.edu.vn>
2021-10-11 22:46:39 +0700

    Udapte

---
+++ LibraryManagement/ViewModel/UserViewModel.cs
@@ -0,0 +1,21 @@
+﻿using LibraryManagement.Model;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LibraryManagement.ViewModel
+{
+    public class UserViewModel : BaseViewModel
+    {
+        private ObservableCollection<User> _UserList;
+        public ObservableCollection<User> UserList { get => _UserList; set { _UserList = value; OnPropertyChanged(); } }
+        public UserViewModel()
+        {
+            UserList=new ObservableCollection<User>(DataProvider.Ins.DB.Users.ToList());
+        }
+
+    }
+}
