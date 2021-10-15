b62a90fb592cf7f2b2b05e2d53ac502b175c21bb
tdn352001 <tdn352001@gmail.com>
2021-10-15 11:00:54 +0700

    upload

---
+++ LibraryManagement/Windows/HistoryStore.xaml.cs
@@ -0,0 +1,38 @@
+﻿using LibraryManagement.Model;
+using LibraryManagement.ViewModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace LibraryManagement.Windows
+{
+    /// <summary>
+    /// Interaction logic for HistoryStore.xaml
+    /// </summary>
+    public partial class HistoryStore : Window
+    {
+        HistoryStoreViewModel viewModel;
+
+        public HistoryStore()
+        {
+            InitializeComponent();
+        }
+
+        public HistoryStore(BookStore bookStore)
+        {
+            InitializeComponent();
+            viewModel = new HistoryStoreViewModel(bookStore);
+            this.DataContext = viewModel;
+        }
+    }
+}
