ec55b57f757e558575894c1a4e95a17e3fcdf8af
tdn352001 <tdn352001@gmail.com>
2021-11-14 20:34:26 +0700

    rebase

---
+++ LibraryManagement/ViewModel/FeeViewModel.cs
@@ -0,0 +1,96 @@
+﻿using LibraryManagement.Model;
+using LibraryManagement.Windows;
+using System;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+namespace LibraryManagement.ViewModel
+{
+    public class FeeViewModel : BaseViewModel
+    {
+        private ObservableCollection<Fee> _FeeList { get; set; }
+        public ObservableCollection<Fee> FeeList { get => _FeeList; set { _FeeList = value; OnPropertyChanged(); } }
+
+        private Fee _FeeSelectedItem;
+        public Fee FeeSelectedItem
+        {
+            get => _FeeSelectedItem;
+            set
+            {
+                _FeeSelectedItem = value;
+                canEditFeeSelectedItem = CanEditFeeSelectedItem();
+                OnPropertyChanged();
+            }
+        }
+
+        private bool _canEditFeeSelectedItem;
+        public bool canEditFeeSelectedItem
+        {
+            get => _canEditFeeSelectedItem;
+            set
+            {
+                _canEditFeeSelectedItem = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public ICommand EditFeeCommand { get; set; }
+        public ICommand FeeDeleteCommand { get; set; }
+
+
+
+        private ObservableCollection<DetailFee> _DetailFeeList { get; set; }
+        public ObservableCollection<DetailFee> DetailFeeList { get => _DetailFeeList; set { _DetailFeeList = value; OnPropertyChanged(); } }
+
+
+
+
+        public FeeViewModel()
+        {
+            FeeList = new ObservableCollection<Fee>(DataProvider.Ins.DB.Fees);
+            canEditFeeSelectedItem = CanEditFeeSelectedItem();
+
+            // Edit Fee Commnad
+            EditFeeCommand = new RelayCommand<Object>((p) => {
+                return canEditFeeSelectedItem;
+            },
+            (p) =>
+            {
+                MessageBox.Show("Helllo");                                                   
+            });
+
+            // Delete Fee Command
+            FeeDeleteCommand = new RelayCommand<Object>((p) => {
+                return canEditFeeSelectedItem;
+            },
+            (p) =>
+            {
+                MessageBox.Show("Helllo");
+            });
+        }
+
+
+
+
+        private bool CanEditFeeSelectedItem()
+        {
+            if (FeeSelectedItem == null)
+                return false;
+            int currentYear = DateTime.UtcNow.Year;
+            int selectedYear = FeeSelectedItem.Interval.Value.Year;
+            
+            if (selectedYear < currentYear)
+                return false;
+
+            int currentMonth = DateTime.UtcNow.Month;
+            int selectedMonth = FeeSelectedItem.Interval.Value.Month;
+            if(selectedYear == currentYear)
+                if (currentMonth >= selectedMonth)
+                    return false;
+
+            return true;
+        }
+    }
+}
--- LibraryManagement/ViewModel/UserViewModel.cs
+++ LibraryManagement/ViewModel/UserViewModel.cs
@@ -5,8 +5,12 @@ using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using System.Windows.Controls;
+using System.Windows;
 using System.Windows.Input;
+using Microsoft.Office.Interop.Excel;
+using Microsoft.Win32;
+using System.Windows.Controls;
+
 
 namespace LibraryManagement.ViewModel
 {
@@ -16,10 +20,107 @@ namespace LibraryManagement.ViewModel
         private ObservableCollection<User> _UserList;
         public ObservableCollection<User> UserList { get => _UserList; set { _UserList = value; OnPropertyChanged(); } }
 
-        // Danh sách lịch sử mượn sách của các độc giả
-        private ObservableCollection<HistoryBook> _HistoryBookList;
-       
-        public ObservableCollection<HistoryBook> HistoryBookList { get => _HistoryBookList; set { _HistoryBookList = value; OnPropertyChanged(); } }
+        private ObservableCollection<UserStatu> _UserStatus;
+        public ObservableCollection<UserStatu> UserStatus { get => _UserStatus; set { _UserStatus = value; OnPropertyChanged(); } }
+
+        private User _SelectedItem;
+        public User SelectedItem
+        {
+            get => _SelectedItem;
+            set
+            {
+                _SelectedItem = value;
+
+                OnPropertyChanged();
+                if (SelectedItem != null)
+                {
+                    Id = SelectedItem.Id;
+                    Name = SelectedItem.Name;
+                    BirthDay = SelectedItem.BirthDay;
+                    Email = SelectedItem.Email;
+                    Address = SelectedItem.Address;
+                    Phone = SelectedItem.Phone;
+                    SelectedUserStatus = SelectedItem.UserStatu;
+                }
+
+            }
+        }
+
+        private UserStatu _SelectedUserStatus;
+        public UserStatu SelectedUserStatus
+        {
+            get => _SelectedUserStatus;
+            set
+            {
+                _SelectedUserStatus = value;
+
+                OnPropertyChanged();
+            }
+        }
+
+        private User _AddItem;
+        public User AddItem
+        {
+            get => _AddItem;
+            set
+            {
+                _AddItem = value;
+                OnPropertyChanged();
+                if (AddItem != null)
+                {
+                    SelectedUserStatus = AddItem.UserStatu;
+                }
+            }
+        }
+
+        private int _Id;
+        public int Id { get => _Id; set { _Id = value; OnPropertyChanged(); } }
+
+        private string _Name;
+        public string Name { get => _Name; set { _Name = value; OnPropertyChanged(); } }
+
+        private DateTime? _BirthDay;
+        public DateTime? BirthDay { get => _BirthDay; set { _BirthDay = value; OnPropertyChanged(); } }
+
+        private string _Email;
+        public string Email { get => _Email; set { _Email = value; OnPropertyChanged(); } }
+
+        private string _Address;
+        public string Address { get => _Address; set { _Address = value; OnPropertyChanged(); } }
+
+        private string _Phone;
+        public string Phone { get => _Phone; set { _Phone = value; OnPropertyChanged(); } }
+
+
+
+
+
+
+
+
+
+
+        // kiểm tra thông tin đang hiển thị là của item được chọn hay của item muốn thêm
+        private bool _AddLayoutVisible = false;
+        public bool AddLayoutVisible { get => _AddLayoutVisible; set { _AddLayoutVisible = value; OnPropertyChanged(); } }
+
+        //Command xử lí khi click vào nút thêm (nút trên list view)
+        public ICommand AddCommand { get; set; }
+
+        // Command xử lí khi ấn vào nút lưu trong layout thêm item
+        public ICommand SaveAddCommand { get; set; }
+
+        // Command xử lí khi ấn vào nút hủy trong layout thêm item
+        public ICommand CancelAddCommand { get; set; }
+
+        // Command xử lí khi ấn vào nút lưu trong layout selected item
+        public ICommand EditCommand { get; set; }
+
+        // Command xử lí khi ấn vào nút xóa trong layout selected item
+        public ICommand DeleteCommand { get; set; }
+
+        // Command xử lí khi ấn vào nút xuất thông tin
+        public ICommand ExportInfoCommand { get; set; }
 
         // thứ tự của heading được chọn trong phân loại tìm kiếm trong commbo box
         private int _SearchHeading = 1;
@@ -30,19 +131,67 @@ namespace LibraryManagement.ViewModel
 
         public UserViewModel()
         {
+
             // lấy danh sách độc giả từ database
             UserList = new ObservableCollection<User>(DataProvider.Ins.DB.Users.ToList());
-            
-            // lấy danh sách lịch sử mượn sách của độc giả từ database
-            HistoryBookList = new ObservableCollection<HistoryBook>(DataProvider.Ins.DB.HistoryBooks.ToList());
+            UserStatus = new ObservableCollection<UserStatu>(DataProvider.Ins.DB.UserStatus);
 
-            //
-            KeyWordChangeCommand = new RelayCommand<TextBox>((p) => { return true; },
+            EditCommand = new RelayCommand<Object>((p) => { return true; },
+                                                             (p) =>
+                                                             {
+                                                                 if (UserValidate(SelectedItem))
+                                                                     SaveChangeUser();
+                                                             });
+
+            AddCommand = new RelayCommand<Object>((p) => { return true; },
+                                                              (p) =>
+                                                              {
+                                                                  AddLayoutVisible = true;
+                                                                  AddItem = new User();
+                                                                  DateTime now = DateTime.Now;
+                                                                  AddItem.BirthDay = now;
+                                                                  OnPropertyChanged("AddItem");
+
+                                                              });
+
+
+            CancelAddCommand = new RelayCommand<Object>((p) => { return true; },
+                                                               (p) =>
+                                                               {
+                                                                   AddLayoutVisible = false;
+                                                                   AddItem = null;
+
+                                                               });
+
+            SaveAddCommand = new RelayCommand<Object>((p) => { return true; },
+                                                             (p) =>
+                                                             {
+                                                                 if (UserValidate(AddItem))
+                                                                     SaveAddUser();
+                                                             });
+
+            DeleteCommand = new RelayCommand<System.Windows.Controls.TextBox>((p) => { return true; },
+                                                             (p) =>
+                                                             {
+                                                                 DeleteUser();
+                                                             });
+
+            ExportInfoCommand = new RelayCommand<Object>((p) => { return true; },
+                                                             (p) =>
+                                                             {
+                                                                 ExportUser();
+                                                             });
+
+
+
+
+            KeyWordChangeCommand = new RelayCommand<System.Windows.Controls.TextBox>((p) => { return true; },
                                                             (p) =>
                                                             {
                                                                 DisplayResultSearch(p.Text);
                                                             });
         }
+
 
         private void DisplayResultSearch(string keyWord)
         {
@@ -64,6 +213,125 @@ namespace LibraryManagement.ViewModel
                         break;
                 }
             }
+        }
+
+        private void SaveChangeUser()
+        {
+            if (SelectedItem != null)
+            {
+                var user = DataProvider.Ins.DB.Users.Where(x => x.Id == SelectedItem.Id).SingleOrDefault();
+                ////SelectedItem.IdStatus = SelectedUserStatus.Id;
+
+                user.Name = Name;
+                user.BirthDay = BirthDay;
+                user.Address = Address;
+                user.Email = Email;
+                user.Phone = Phone;
+                user.IdStatus = SelectedUserStatus.Id;
+                DataProvider.Ins.DB.SaveChanges();
+
+                SelectedItem.Name = Name;
+                MessageBox.Show("Đã Lưu", "Thành Công", MessageBoxButton.OK, MessageBoxImage.Information);
+            }
+        }
+
+        private void SaveAddUser()
+        {
+            if (AddItem != null)
+            {
+                DataProvider.Ins.DB.Users.Add(AddItem);
+                AddItem.IdStatus = 1;
+                DataProvider.Ins.DB.SaveChanges();
+                SelectedItem = AddItem;
+                UserList.Add(AddItem);
+                AddItem = null;
+                AddLayoutVisible = false;
+            }
+        }
+
+
+        private Boolean UserValidate(User user)
+        {
+            if (user == null)
+                return false;
+
+            if (String.IsNullOrEmpty(user.Name))
+                return false;
+
+            if (String.IsNullOrEmpty(user.Address))
+                return false;
+
+            return true;
+        }
+
+        private void DeleteUser()
+        {
+            if (SelectedItem == null) return;
+
+            String title = "Xóa thành viên ?";
+            String message = "Hành động này không thể khôi phục, Bạn có chắc chắn?";
+            MessageBoxResult dialogResult = MessageBox.Show(message, title, MessageBoxButton.YesNo);
+            if (dialogResult == MessageBoxResult.Yes)
+            {
+                if (SelectedItem.DetailFees.Count > 0)
+                {
+                    String notifyTitle = "Thông báo";
+                    String notifyMessage = "Không thể xóa thành viên này.";
+                    MessageBox.Show(notifyMessage, notifyTitle, MessageBoxButton.OK);
+                }
+                else
+                {
+
+                    DataProvider.Ins.DB.Users.Remove(SelectedItem);
+                    SelectedItem.IdStatus = SelectedUserStatus.Id;
+                    DataProvider.Ins.DB.SaveChanges();
+                    UserList.Remove(SelectedItem);
+                    String notifyTitle = "Thông báo";
+                    String notifyMessage = "Xóa thành công.";
+                    MessageBox.Show(notifyMessage, notifyTitle, MessageBoxButton.OK);
+                }
+            }
+
+
+        }
+
+        private void ExportUser()
+        {
+            SaveFileDialog sfd = new SaveFileDialog() { Filter = "Excel Workbook |*.xls", ValidateNames = true };
+            if (sfd.ShowDialog() == true)
+            {
+
+                Microsoft.Office.Interop.Excel.Application app = new Microsoft.Office.Interop.Excel.Application();
+                Workbook wb = app.Workbooks.Add(XlSheetType.xlWorksheet);
+                Worksheet ws = (Worksheet)app.ActiveSheet;
+                app.Visible = false;
+                ws.Cells[1, 1] = "Mã độc giả";
+                ws.Cells[1, 2] = "Tên độc giả";
+                ws.Cells[1, 3] = "Ngày sinh";
+                ws.Cells[1, 4] = "Email";
+                ws.Cells[1, 5] = "Địa chỉ";
+                ws.Cells[1, 6] = "SĐT";
+                ws.Cells[1, 7] = "Trạng thái";
+                int i = 2;
+                foreach (var item in UserList)
+                {
+                    ws.Cells[i, 1] = item.Id;
+                    ws.Cells[i, 2] = item.Name;
+                    ws.Cells[i, 3] = item.BirthDay;
+                    ws.Cells[i, 4] = item.Email;
+                    ws.Cells[i, 5] = item.Address;
+                    ws.Cells[i, 6] = item.Phone;
+                    ws.Cells[i, 7] = item.UserStatu.Name;
+                    i++;
+                }
+                wb.SaveAs(sfd.FileName, XlFileFormat.xlWorkbookDefault, Type.Missing, Type.Missing, true, false, XlSaveAsAccessMode.xlNoChange, XlSaveConflictResolution.xlLocalSessionChanges, Type.Missing, Type.Missing);
+                app.Quit();
+                MessageBox.Show("Your data has been successfully exported. ");
+
+
+            }
+
+
         }
 
     }
