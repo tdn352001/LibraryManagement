a8e4315742a9459dce3c311d7011d14a23391d5e
tdn352001 <tdn352001@gmail.com>
2021-11-15 21:53:26 +0700

    fee control

--- LibraryManagement/ViewModel/FeeViewModel.cs
+++ LibraryManagement/ViewModel/FeeViewModel.cs
@@ -25,6 +25,31 @@ namespace LibraryManagement.ViewModel
             }
         }
 
+        private Fee _AddFeeItem;
+        public Fee AddFeeItem
+        {
+            get => _AddFeeItem;
+            set
+            {
+                _AddFeeItem = value;
+                OnPropertyChanged();
+            }
+        }
+
+        private DateTime _AddFeeDate;
+        public DateTime AddFeeDate
+        {
+            get => _AddFeeDate;
+            set
+            {
+                DateTime selectedDate = new DateTime(value.Year, value.Month, 1);
+                _AddFeeDate = selectedDate;
+                if (AddFeeItem != null)
+                    AddFeeItem.Interval = selectedDate;
+                OnPropertyChanged();
+            }
+        }
+
         private bool _canEditFeeSelectedItem;
         public bool canEditFeeSelectedItem
         {
@@ -36,8 +61,15 @@ namespace LibraryManagement.ViewModel
             }
         }
 
+        // kiểm tra thông tin đang hiển thị là của item được chọn hay của item muốn thêm
+        private bool _AddFeeLayoutVisible = false;
+        public bool AddFeeLayoutVisible { get => _AddFeeLayoutVisible; set { _AddFeeLayoutVisible = value; OnPropertyChanged(); } }
+
+        public ICommand AddFeeCommand { get; set; }
+        public ICommand SaveAddFeeCommand { get; set; }
+        public ICommand CancelAddFeeCommand { get; set; }
         public ICommand EditFeeCommand { get; set; }
-        public ICommand FeeDeleteCommand { get; set; }
+        public ICommand DeleteFeeCommand { get; set; }
 
 
 
@@ -52,45 +84,148 @@ namespace LibraryManagement.ViewModel
             FeeList = new ObservableCollection<Fee>(DataProvider.Ins.DB.Fees);
             canEditFeeSelectedItem = CanEditFeeSelectedItem();
 
+
+            //Add Fee Command
+            AddFeeCommand = new RelayCommand<Object>((p) => { return true; },
+            (p) =>
+            {
+                AddFeeLayoutVisible = true;
+                AddFeeItem = new Fee();
+                DateTime now = DateTime.Now;
+                DateTime nextMonth = now.AddMonths(1);
+                AddFeeDate = new DateTime(nextMonth.Year, nextMonth.Month, 1);
+                OnPropertyChanged("AddFeeItem");
+
+            });
+
+
+            CancelAddFeeCommand = new RelayCommand<Object>((p) => { return true; },
+            (p) =>
+            {
+                AddFeeLayoutVisible = false;
+                AddFeeItem = null;
+            });
+
+
+            // Save Add Fee Command
+            SaveAddFeeCommand = new RelayCommand<Object>(
+            (p) => {
+                DateTime now = DateTime.Now;
+                DateTime CurrentMonth = new DateTime(now.Year, now.Month, 1);
+                return DateTime.Compare(CurrentMonth, AddFeeDate) < 0 && !CheckExistDateInDB();
+            },
+            (p) => {
+                if (AddFeeItem.Money >= 0)
+                {
+                    AddFee();
+                }
+                else
+                {
+                    MessageBox.Show("Số tiền phải là số không âm", "Lỗi", MessageBoxButton.OK, MessageBoxImage.Warning);
+                }
+
+            });
+
             // Edit Fee Commnad
-            EditFeeCommand = new RelayCommand<Object>((p) => {
+            EditFeeCommand = new RelayCommand<Object>((p) =>
+            {
                 return canEditFeeSelectedItem;
             },
             (p) =>
             {
-                MessageBox.Show("Helllo");                                                   
+                if (FeeSelectedItem.Money >= 0)
+                {
+                    SaveChangeFee();
+                }
+                else
+                {
+                    MessageBox.Show("Số tiền phải là số không âm", "Lỗi", MessageBoxButton.OK, MessageBoxImage.Warning);
+                }
             });
 
             // Delete Fee Command
-            FeeDeleteCommand = new RelayCommand<Object>((p) => {
+            DeleteFeeCommand = new RelayCommand<Object>((p) =>
+            {
                 return canEditFeeSelectedItem;
             },
             (p) =>
             {
-                MessageBox.Show("Helllo");
+                DeleteFee();
             });
         }
 
+       
 
 
-
+        // Check Fee Selected Item Can Change
         private bool CanEditFeeSelectedItem()
         {
             if (FeeSelectedItem == null)
                 return false;
             int currentYear = DateTime.UtcNow.Year;
             int selectedYear = FeeSelectedItem.Interval.Value.Year;
-            
+
             if (selectedYear < currentYear)
                 return false;
 
             int currentMonth = DateTime.UtcNow.Month;
             int selectedMonth = FeeSelectedItem.Interval.Value.Month;
-            if(selectedYear == currentYear)
+            if (selectedYear == currentYear)
                 if (currentMonth >= selectedMonth)
                     return false;
 
             return true;
+        }
+
+        private bool CheckExistDateInDB()
+        {
+            var countDate = DataProvider.Ins.DB.Fees.Where(x => DateTime.Compare(AddFeeDate, (DateTime)x.Interval) == 0).Count();
+            return countDate > 0;
+        }
+
+
+        // Save Add Fee
+        private void AddFee()
+        {
+            if(AddFeeItem != null)
+            {
+                DataProvider.Ins.DB.Fees.Add(AddFeeItem);
+                DataProvider.Ins.DB.SaveChanges();
+                FeeSelectedItem = AddFeeItem;
+                FeeList.Add(AddFeeItem);
+                AddFeeItem = null;
+                AddFeeLayoutVisible = false;
+                MessageBox.Show("Thêm thành công", "Thành công", MessageBoxButton.OK, MessageBoxImage.Information);
+            }
+        }
+
+        // Save Change FeeItem
+        private void SaveChangeFee()
+        {
+            var fee = DataProvider.Ins.DB.Fees.Where(x => x.Id == FeeSelectedItem.Id).SingleOrDefault();
+            fee.Money = FeeSelectedItem.Money;
+            DataProvider.Ins.DB.SaveChanges();
+            MessageBox.Show("Đã Lưu", "Lưu Thành Công", MessageBoxButton.OK, MessageBoxImage.Information);
+        }
+
+        // Delete Fee Item
+        private void DeleteFee()
+        {
+            if (FeeSelectedItem == null)
+                return;
+
+            String title = "Xóa?";
+            String message = "Hành động này không thể khôi phục, Bạn có chắc chắn?";
+            MessageBoxResult dialogResult = MessageBox.Show(message, title, MessageBoxButton.YesNo, MessageBoxImage.Information);
+            if (dialogResult == MessageBoxResult.Yes)
+            {
+                DataProvider.Ins.DB.Fees.Remove(FeeSelectedItem);
+                DataProvider.Ins.DB.SaveChanges();
+                FeeList.Remove(FeeSelectedItem);
+                String notifyTitle = "Thông báo";
+                String notifyMessage = "Xóa thành công.";
+                MessageBox.Show(notifyMessage, notifyTitle, MessageBoxButton.OK);
+            }
         }
     }
 }
